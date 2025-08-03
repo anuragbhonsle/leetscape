0550892565d89ee7bc02ca9d4b278f1973ff571d
AnuragBhonsle <anuragkbhonsle@gmail.com>
2025-08-04 05:21:14 +0530

    Footer logo removed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -15,12 +15,7 @@ export function Footer() {
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           {/* Brand */}
           <div className="space-y-4">
-            <div className="flex items-center space-x-3">
-              <img
-                src="/logo.png"
-                alt="LeetScape"
-                className="h-8 w-8 object-contain"
-              />
+            <div className="flex items-center">
               <span className="text-lg font-semibold text-foreground">
                 LeetScape
               </span>
